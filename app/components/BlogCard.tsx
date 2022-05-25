869240919164665efac90198898d0d3f729ddad3
Trreffy <ricalisboa@gmail.com>
2022-05-24 23:10:41 -0300

    blog cards adapted to light theme

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -16,14 +16,14 @@ type Props = {
 const BlogCard = ({ name, date, readTime, tags }: Props) => {
   return (
     <Link to="/cool">
-      <div className="group h-[300px] w-[350px] cursor-pointer bg-neutral-800 rounded-tl-3xl rounded-br-3xl hover:scale-110 transition-transform duration-[400ms]">
+      <div className="group h-[300px] w-[350px] cursor-pointer bg-neutral-200 dark:bg-neutral-800 rounded-tl-3xl rounded-br-3xl hover:scale-110 transition-transform duration-[400ms]">
         <div className="w-full h-[150px] rounded-tl-3xl bg-gradient-to-tl from-[#f89720] to-[#fed006] group-hover:h-[190px] transition-all duration-[400ms]">
-          <div className="w-5 h-5 ml-auto mr-2.5 pt-2.5 fill-white opacity-0 group-hover:opacity-100 group-hover:drop-shadow-[0_0_1px_#fff] transition duration-[400ms]">
+          <div className="w-5 h-5 ml-auto mr-2.5 pt-2.5 fill-neutral-800 dark:fill-neutral-100 opacity-0 group-hover:opacity-100 group-hover:drop-shadow-[0_0_1px_#404040] dark:group-hover:drop-shadow-[0_0_1px_#fff] transition duration-[400ms]">
             {tags}
           </div>
         </div>
         <div className="px-5 py-2.5 h-[150px] flex flex-col justify-between group-hover:h-[110px] transition-all duration-[400ms]">
-          <h1 className="text-lg text-neutral-100 font-semibold">{name}</h1>
+          <h1 className="text-lg text-neutral-800 dark:text-neutral-100 font-semibold">{name}</h1>
           <footer className="flex flex-row justify-between">
             <p className="text-lg text-neutral-400 font-semibold">{date}</p>
             <div className="flex flex-row w-16 justify-between items-center">
--- app/components/Pill.tsx
+++ app/components/Pill.tsx
@@ -11,7 +11,7 @@ const Pill = ({ name, icon }: Props) => {
     const [active, setActive] = useState<boolean>(false)
   return (
     <div onClick={() => setActive(!active)} className={`group select-none flex flex-row justify-between ${active && "bg-neutral-800/[0.35] border-neutral-800 dark:bg-neutral-200/[0.15] dark:border-neutral-200"} p-2.5 rounded bg-neutral-300 dark:bg-neutral-800 border border-transparent hover:border-neutral-700 dark:hover:border-neutral-500 transtion-all duration-150 cursor-pointer`}>
-      <div className={`w-8 h-8 ${active && "fill-neutral-800 dark:fill-neutral-200"} mr-2 fill-neutral-700 dark:fill-neutral-500 group-hover:fill-neutral-800  dark:group-hover:fill-neutral-100 group-hover:drop-shadow-[0_0_2px_#404040] dark:group-hover:drop-shadow-[0_0_1px_#fff] transition-all duration-150`}>{icon}</div>
+      <div className={`w-8 h-8 ${active && "fill-neutral-800 dark:fill-neutral-200"} mr-2 fill-neutral-700 dark:fill-neutral-500 group-hover:fill-neutral-800  dark:group-hover:fill-neutral-100 group-hover:drop-shadow-[0_0_1px_#404040] dark:group-hover:drop-shadow-[0_0_1px_#fff] transition-all duration-150`}>{icon}</div>
       <p className={`${active && "text-neutral-800 dark:text-neutral-200"} text-neutral-500 text-2xl font-regular mx-2 group-hover:text-neutral-800 dark:group-hover:text-neutral-100 transition-all duration-150`}>{name}</p>
     </div>
   );
