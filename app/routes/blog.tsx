6df3137ac0275982f1f5cb60e206d83c6d63c4a6
Trreffy <ricalisboa@gmail.com>
2022-05-27 10:50:21 -0300

    testing

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,8 +1,17 @@
-// import styles from "highlight.js/styles/github-dark-dimmed.css";
 import { Outlet } from "@remix-run/react";
+import { MetaFunction, LoaderFunction } from "@remix-run/node";
 import Navbar from "~/components/Navbar";
+import { useLocation } from 'react-router-dom';
+import generateSocialPreview from "~/utils/generateSocialPreview";
+
+export const meta: MetaFunction = () => {
+  return {
+    ...generateSocialPreview({ url: `https://simplifiedknowledge.vercel.app/${useLocation().pathname}`})
+  };
+// };
 
 export default function Blog(props: any) {
+  
   return (
     <div className="w-full min-h-screen h-full bg-neutral-100 dark:bg-neutral-800">
       <Navbar />
