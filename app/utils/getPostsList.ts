cc7492b8175e8c616108dd12460dce7811dbf250
Trreffy <ricalisboa@gmail.com>
2022-06-05 15:25:43 -0300

    novo post

--- app/utils/getPostsList.ts
+++ app/utils/getPostsList.ts
@@ -3,8 +3,9 @@ import * as dryCodePost from "../routes/blog/dry-code-controversies.mdx";
 
 import * as postReferencia from "../routes/blog/referencia-vs-valor.mdx";
 import * as postDryCode from "../routes/blog/controversias-dry-code.mdx";
+import * as middlewareNoExpress from "../routes/blog/middlewares-no-express.mdx";
 
-let posts = [referencePost, postReferencia, dryCodePost, postDryCode];
+let posts = [referencePost, postReferencia, dryCodePost, postDryCode, middlewareNoExpress];
 
 const getPosts = () => {
   return posts;
