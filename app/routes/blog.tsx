657c7e8e61441c541fda31cbf76aec2440ef0634
Henrique Torres <ricalisboa@gmail.com>
2022-10-22 17:38:29 -0300

    added a short description to each post

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,9 +1,9 @@
-import { Outlet } from "@remix-run/react";
-import type { MetaFunction } from "@remix-run/node";
-import { useLocation } from "react-router-dom";
+import { Outlet } from '@remix-run/react';
+import type { MetaFunction } from '@remix-run/node';
+import { useLocation } from 'react-router-dom';
 
-import { Navbar, Footer } from "@components/global";
-import generateSocialPreview from "@utils/generateSocialPreview";
+import { Navbar, Background } from '@components/global';
+import generateSocialPreview from '@utils/generateSocialPreview';
 
 export const meta: MetaFunction = () => ({
   ...generateSocialPreview({
@@ -14,12 +14,13 @@ export const meta: MetaFunction = () => ({
 
 const Blog = () => {
   return (
-    <div className="w-full min-h-screen h-full bg-neutral-100 dark:bg-neutral-900">
+    <>
+      <Background />
       <Navbar />
-      <article className="prose lg:prose-xl mx-auto prose-neutral dark:prose-invert py-40 px-2">
+      <article className='prose lg:prose-xl mx-auto prose-neutral dark:prose-invert py-40 px-2'>
         <Outlet />
       </article>
-    </div>
+    </>
   );
 };
 
